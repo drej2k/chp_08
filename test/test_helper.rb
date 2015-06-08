e16e6f4294b477ad1e8fc9511af4b9bc211131db
drej2k <andrevidic@hotmail.com>
2015-06-09 01:46:34 +1000

    current user tests added to sessions_helper_test.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -29,8 +29,8 @@ class ActiveSupport::TestCase
   end
 
   private
-  # Returns true inside an integration test.
-  def integration_test?
-    defined?(post_via_redirect)
-  end
+    # Returns true inside an integration test.
+    def integration_test?
+      defined?(post_via_redirect)
+    end
 end
